f4d541fa89fd5ff7651824cd6b927c8c653bf4ab
Daniel Obłąk <daniel.oblak99@gmail.com>
2023-04-21 21:10:02 +0200

    Unfinished implementation EditBookDialog

--- lib/explorer/widgets/dialogs/edit_book_dialog.dart
+++ lib/explorer/widgets/dialogs/edit_book_dialog.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:developer';
+
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ztp_projekt/authors/controllers/form/authors_form_state.dart';
-import 'package:ztp_projekt/authors/controllers/manage_authors/manage_authors_state.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:ztp_projekt/authors/controllers/providers.dart';
 import 'package:ztp_projekt/authors/models/author.dart';
 import 'package:ztp_projekt/books/controllers/form/book_form_state.dart';
@@ -37,6 +40,7 @@ class EditBookDialog extends StatefulWidget {
 class _EditBookDialogState extends State<EditBookDialog> {
   late TextEditingController _titleController;
   late TextEditingController _publisherController;
+  late TextEditingController _dateController;
   late TextEditingController _isbnNumberController;
   late TextEditingController _priceController;
 
@@ -44,6 +48,9 @@ class _EditBookDialogState extends State<EditBookDialog> {
   void initState() {
     _titleController = TextEditingController(text: widget.book.title);
     _publisherController = TextEditingController(text: widget.book.publisher);
+    _dateController = TextEditingController(
+      text: DateFormat('dd-MM-yyyy').format(widget.book.publicationDate),
+    );
     _isbnNumberController = TextEditingController(text: widget.book.isbnNumber);
     _priceController =
         TextEditingController(text: widget.book.price.toString());
@@ -91,7 +98,7 @@ class _EditBookDialogState extends State<EditBookDialog> {
                 SizedBox(
                   height: MediaQuery.of(context).size.height * 0.45,
                   width: MediaQuery.of(context).size.width * 0.30,
-                  child: Column(
+                  child: ListView(
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(bottom: 10),
@@ -115,11 +122,27 @@ class _EditBookDialogState extends State<EditBookDialog> {
                         ),
                         child: DropdownButton2<Author>(
                           isExpanded: true,
+                          onChanged: (author) {
+                            if (author != null) {
+                              bookFormNotifier.updateAuthor(author);
+                            }
+                          },
+                          underline: const SizedBox(),
+                          buttonStyleData: ButtonStyleData(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(14),
+                            ),
+                          ),
+                          dropdownStyleData: DropdownStyleData(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(14),
+                            ),
+                          ),
                           value: bookFormState.author,
                           items: getAuthorsState.authors
                               .map(
                                 (author) => DropdownMenuItem<Author>(
-                                  onTap: () {},
+                                  value: author,
                                   child: Text(author.fullName),
                                 ),
                               )
@@ -142,20 +165,95 @@ class _EditBookDialogState extends State<EditBookDialog> {
                           },
                         ),
                       ),
-                      FilledButton.tonalIcon(
-                        style: FilledButton.styleFrom(
-                          shape: const RoundedRectangleBorder(),
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: TextFormField(
+                            readOnly: true,
+                            controller: _dateController,
+                            decoration: InputDecoration(
+                              labelText: 'Publication date',
+                              errorText: _dateController.text.isNotEmpty
+                                  ? null
+                                  : 'Field cannot be empty',
+                            ),
+                            onTap: () async {
+                              final pickedDate = await _showTableCalendar(
+                                context,
+                                bookFormState.publicationDate!,
+                              );
+                              log('result:$pickedDate');
+                              if (pickedDate != null) {
+                                bookFormNotifier
+                                    .updatePublicationDate(pickedDate);
+                                setState(() {
+                                  _dateController.text =
+                                      DateFormat('dd-MM-yyyy')
+                                          .format(pickedDate);
+                                });
+                              }
+                            },
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: TextFormField(
+                          controller: _isbnNumberController,
+                          decoration: InputDecoration(
+                            labelText: 'ISBN number',
+                            errorText: _isbnNumberController.text.isNotEmpty
+                                ? null
+                                : 'Field cannot be empty',
+                          ),
+                          onChanged: (isbnNumber) {
+                            bookFormNotifier.updateIsbnNumber(isbnNumber);
+                            setState(() {});
+                          },
                         ),
-                        onPressed: () async {
-                          await _editOnPressed(ref, context);
-                        },
-                        icon: const Icon(
-                          Icons.edit,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: TextFormField(
+                          controller: _priceController,
+                          decoration: InputDecoration(
+                            labelText: 'Price',
+                            errorText: _priceController.text.isNotEmpty
+                                ? null
+                                : 'Field cannot be empty',
+                          ),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(
+                              RegExp('^[0-9]+\\.?[0-9]*'),
+                            ),
+                            FilteringTextInputFormatter.deny(
+                              RegExp('[a-z]'),
+                            ),
+                            FilteringTextInputFormatter.deny(
+                              RegExp(','),
+                            )
+                          ],
+                          onChanged: (price) {
+                            if (price.isNotEmpty) {
+                              bookFormNotifier.updatePrice(double.parse(price));
+                            } else {
+                              bookFormNotifier.updatePrice(null);
+                            }
+                            setState(() {});
+                          },
                         ),
-                        label: const Text('Edit'),
                       ),
                     ],
                   ),
+                ),
+                FilledButton.tonalIcon(
+                  style: FilledButton.styleFrom(
+                    shape: const RoundedRectangleBorder(),
+                  ),
+                  onPressed: () async {
+                    await _editOnPressed(ref, context);
+                  },
+                  icon: const Icon(
+                    Icons.edit,
+                  ),
+                  label: const Text('Edit'),
                 ),
               ],
             ),
@@ -163,6 +261,51 @@ class _EditBookDialogState extends State<EditBookDialog> {
         );
       },
     );
+  }
+
+  Future<DateTime?> _showTableCalendar(
+    BuildContext context,
+    DateTime focusedDay,
+  ) async {
+    final pickedDate = await showDialog<DateTime?>(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: SizedBox(
+          height: 400,
+          width: 400,
+          child: TableCalendar(
+            locale: 'pl_PL',
+            focusedDay: focusedDay,
+            firstDay: DateTime(1950),
+            lastDay: DateTime.now(),
+            currentDay: DateTime.now(),
+            calendarStyle: const CalendarStyle(
+              selectedDecoration: BoxDecoration(
+                color: Colors.amber,
+                shape: BoxShape.circle,
+              ),
+              todayDecoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              todayTextStyle: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+            headerStyle: const HeaderStyle(
+              formatButtonVisible: false,
+            ),
+            onDaySelected: (selectedDay, focusedDay) {
+              Navigator.of(context).pop(selectedDay);
+            },
+            
+            onFormatChanged: (_) {},
+          ),
+        ),
+      ),
+    );
+    // TODO: Replace TableCalendar with DatePickerDialog
+    DatePickerDialog(initialDate: initialDate, firstDate: firstDate, lastDate: lastDate)
+    return pickedDate;
   }
 
   Future<void> _editOnPressed(WidgetRef ref, BuildContext context) async {
