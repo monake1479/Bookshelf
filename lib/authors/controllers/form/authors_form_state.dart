2b872fb04079f886889691a34541872f52ac5621
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-04-22 11:55:23 +0200

    Implemented extension isFormValid and toInsertMap which map notifier state

--- lib/authors/controllers/form/authors_form_state.dart
+++ lib/authors/controllers/form/authors_form_state.dart
@@ -22,9 +22,16 @@ class AuthorsFormState with _$AuthorsFormState {
 extension AuthorsFormStateEx on AuthorsFormState {
   bool get isFirstNameValid => firstName != null && firstName!.isNotEmpty;
   bool get isLastNameValid => lastName != null && lastName!.isNotEmpty;
+  bool get isFormValid => isFirstNameValid && isLastNameValid;
   Author get toAuthor => Author(
         id: id!,
         firstName: firstName!,
         lastName: lastName!,
       );
+
+  Map<String, Object?> get toInsertMap => {
+        'id': null,
+        'firstName': firstName,
+        'lastName': lastName,
+      };
 }
